3e4e12a1383d82fce7a732b168cfbc1b686297cb
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-06-24 07:54:32 -0400

    add shell of test runner

--- spec/components/Clock-spec.js
+++ spec/components/Clock-spec.js
@@ -1,13 +1,42 @@
 'use babel';
 
-import {expect} from 'chai';
-import {shallow} from 'enzyme';
+import chai, {expect} from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import {shallow, mount} from 'enzyme';
 import React from 'react';
 import Clock from '../../lib/components/Clock';
+
+chai.use(sinonChai);
 
 describe('<Clock />', () => {
   it('should render', () => {
     const wrapper = shallow(<Clock />);
     expect(wrapper.length).to.equal(1);
+  });
+
+  it('should periodically update the time', () => {
+
+    runs(() => {
+      wrapper = mount(<Clock />);
+      spy = sinon.spy(wrapper.instance(), 'getPrettyTime');
+
+      ready = false;
+      wait = 3000;
+
+      setTimeout(() => {
+        console.log('>>>>>>>>>>>>>set ready');
+        ready = true;
+      });
+    });
+
+    waitsFor(() => {
+      // console.log('??ready', ready);
+      return ready;
+    }, '', wait + wait);
+
+    runs(() => {
+      spy.should.have.been.called;
+    });
   });
 });
