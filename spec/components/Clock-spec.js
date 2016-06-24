7a9fe19136dfda7a1eed28fa6f4f4b1ed5ea7864
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-06-24 07:15:38 -0400

    add mocha, chai

---
+++ spec/components/Clock-spec.js
@@ -0,0 +1,10 @@
+'use babel';
+
+import {expect} from 'chai';
+import Clock from '../../lib/components/Clock';
+
+describe('<Clock />', () => {
+  it('should render', () => {
+    expect(1).to.equal(2);
+  });
+});
